522fefaac3d6104906852ea6d6477ddef0441ad7
marufalaslam <maruf14@cse.pstu.ac.bd>
2024-04-23 18:31:54 +0600

    slider done

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(".vehicle-slider").slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  arrows: true,
+  prevArrow:
+    '<button type="button" class="btn btn-dark prev-btn"><i class="fa fa-arrow-left"></i></button>',
+  nextArrow:
+    '<button type="button" class="btn btn-dark next-btn"><i class="fa fa-arrow-right"></i></button>',
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
